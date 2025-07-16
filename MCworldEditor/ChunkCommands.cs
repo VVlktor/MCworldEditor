4a6144466e255267765fd3c0019a0c97543f4d7b
VVlktor <gieruszczakwiktor@gmail.com>
2025-07-16 19:28:29 +0200

    Add ChunkCommand

---
+++ MCworldEditor/ChunkCommands.cs
@@ -0,0 +1,80 @@
+﻿using fNbt;
+
+namespace MCworldEditor
+{
+    public class ChunkCommands
+    {
+        public int CountBlocksOnChunk(int worldId, int blockId, int x, int z)
+        {
+            string chunkPath = GetChunkPathByCoordinates(worldId, x, 0, z);
+            if (!File.Exists(chunkPath))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine("Chunk hasnt been generated yet.");
+                Console.ResetColor();
+                return 1;
+            }
+
+            NbtFile nbtFile = new NbtFile(chunkPath);
+            using (FileStream stream = File.OpenRead(chunkPath))
+            {
+                nbtFile.LoadFromStream(stream, NbtCompression.AutoDetect);
+            }
+            var levelTag = nbtFile.RootTag.Get<NbtCompound>("Level");
+            var blocks = levelTag!.Get<NbtByteArray>("Blocks")!.Value;
+            int blockCount = blocks.Count(x => x == blockId);
+            Console.WriteLine($"Number of blocks with id {blockId}: {blockCount}");
+            return 0;
+        }
+
+        public string GetChunkPathByCoordinates(int worldNumber, int x, int y, int z)
+        {
+            var coords = GetChunkCoordinatesFromBlock(x, z);
+            var chunkX = coords.chunkX;
+            var chunkZ = coords.chunkZ;
+            string signed36X = ToSignedBase36(chunkX);
+            string signed36Z = ToSignedBase36(chunkZ);
+            string f1 = ToBase36(chunkX);
+            string f2 = ToBase36(chunkZ);
+            string path = Path.Combine(@"C:\Users\Admin\AppData\Roaming\.minecraft\saves\World"+worldNumber, f1, f2, $"c.{signed36X}.{signed36Z}.dat");
+            return path;
+        }
+
+        private (int chunkX, int chunkZ) GetChunkCoordinatesFromBlock(int blockX, int blockZ)
+        {
+            int chunkX = (int)Math.Floor(blockX / 16.0);
+            int chunkZ = (int)Math.Floor(blockZ / 16.0);
+            return (chunkX, chunkZ);
+        }
+
+        private string ToBase36(int v)
+        {
+            int av = (v < 0 ? v + 64 * ((-v + 63) / 64) : v) % 64;
+            return ToSignedBase36(av);
+        }
+
+        private string ToSignedBase36(int value)
+        {
+            const string chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+            if (value == 0) return "0";
+
+            bool isNegative = value < 0;
+            if (isNegative) 
+                value = -value;
+
+            var result = new System.Text.StringBuilder();
+
+            while (value > 0)
+            {
+                int remainder = value % 36;
+                result.Insert(0, chars[remainder]);
+                value /= 36;
+            }
+
+            if (isNegative)
+                result.Insert(0, '-');
+
+            return result.ToString();
+        }
+    }
+}
--- MCworldEditor/Commands.cs
+++ MCworldEditor/Commands.cs
@@ -1,10 +1,13 @@
-﻿using System.CommandLine;
+﻿using Microsoft.VisualBasic.FileIO;
+using System.CommandLine;
+using System.Numerics;
 
 namespace MCworldEditor
 {
     public class Commands
     {
         private InventoryCommands inventoryCommands = new();
+        private ChunkCommands chunkCommands = new();
 
         private RootCommand rootCommand;
         private Argument<int> _worldNumberArgument;
@@ -18,11 +21,39 @@ namespace MCworldEditor
         public void PrepareCommands(string[] args)
         {
             AddInventoryCommands();
+            AddChunkCommands();
         }
 
         public int CallCommand(string[] args)
         {
             return rootCommand.Parse(args).Invoke();
+        }
+
+        public void AddChunkCommands()
+        {
+            Command chunkCommand = new("chunk", "Allows to operate on chunks");
+
+            rootCommand.Subcommands.Add(chunkCommand);
+
+            Argument<int> searchForBlockIdArgument = new("blockId") { 
+                Description = "ID of the block to search for."
+            };
+
+            Option<int> xOption = new("-x")
+            {
+                Description = "In-world X coordinates",
+                Required = true
+            };
+
+            Option<int> zOption = new("-z")
+            {
+                Description = "In-world Z coordinates",
+                Required = true
+            };
+
+            Command searchForBlockCommand = new("count", "Searches for block with specified id on chunk with provided coordinates") { _worldNumberArgument, searchForBlockIdArgument, xOption, zOption };
+            searchForBlockCommand.SetAction(arguments=>chunkCommands.CountBlocksOnChunk(arguments.GetValue(_worldNumberArgument), arguments.GetValue(searchForBlockIdArgument), arguments.GetValue(xOption), arguments.GetValue(zOption)));
+            chunkCommand.Subcommands.Add(searchForBlockCommand);
         }
 
         public void AddInventoryCommands()
@@ -41,9 +72,9 @@ namespace MCworldEditor
                 DefaultValueFactory = count => 1,
                 Description = "Number of items to add"
             };
+            addItemCountOption.Aliases.Add("--amount");
 
             Command addItemCommand = new("add", "Adds item with specified id to inventory") { _worldNumberArgument, addItemArgument };
-            addItemCountOption.Aliases.Add("--amount");
             addItemCommand.Options.Add(addItemCountOption);
             inventoryCommand.Subcommands.Add(addItemCommand);
             addItemCommand.SetAction(arguments => inventoryCommands.AddItemToInventory(arguments.GetValue(_worldNumberArgument), arguments.GetValue(addItemArgument), arguments.GetValue(addItemCountOption)));
@@ -60,6 +91,33 @@ namespace MCworldEditor
             {
                 Description = "Number of world (1-5)"
             };
+
+            worldNumberArgument.Validators.Add(result =>
+            {
+                if (result.Tokens.Count == 0)
+                {
+                    result.AddError("World number is required.");
+                    return;
+                }
+
+                if (!int.TryParse(result.Tokens[0].Value, out int worldNumber))
+                {
+                    result.AddError("World number must be a valid integer.");
+                    return;
+                }
+
+                if (worldNumber > 5 || worldNumber < 1)
+                {
+                    result.AddError("There are only 5 world slots in minecraft beta.");
+                    return;
+                }
+
+                string worldPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".minecraft", "saves", $"World{worldNumber}");
+
+                if (!Directory.Exists(worldPath))
+                    result.AddError("World with specified number does not exist.");
+
+            });
 
             return worldNumberArgument;
         }
